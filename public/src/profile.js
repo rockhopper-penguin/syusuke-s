acd379ee17a6b4265a2237a3e2b58ca9b02783eb
ペンギン <46835999+rockhopper-penguin@users.noreply.github.com>
2020-01-10 09:56:58 +0900

    インデントをタブに変換

--- public/src/profile.js
+++ public/src/profile.js
@@ -1,18 +1,18 @@
 const profile = new Vue({
-    el: '#profile',
-    data: {
-        getAge: this.calcAge(),
-    }
+	el: '#profile',
+	data: {
+		getAge: this.calcAge(),
+	}
 });
 
 function calcAge() {
-    const birthday; //生年月日
-    const getToday = new Date();
-    const getYear = getToday.getFullYear().toString().padStart(4, '0');
-    const getMonth = (getToday.getMonth() + 1).toString().padStart(2, '0');
-    const getDate = getToday.getDate().toString().padStart(2, '0');
-    let adder = getYear + getMonth + getDate;
-    let calc = Math.floor((Number(adder) - birthday) / 10000);
-    console.log(calc);
-    return calc;
+	const birthday; //生年月日
+	const getToday = new Date();
+	const getYear = getToday.getFullYear().toString().padStart(4, '0');
+	const getMonth = (getToday.getMonth() + 1).toString().padStart(2, '0');
+	const getDate = getToday.getDate().toString().padStart(2, '0');
+	let adder = getYear + getMonth + getDate;
+	let calc = Math.floor((Number(adder) - birthday) / 10000);
+	console.log(calc);
+	return calc;
 }
