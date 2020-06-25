f03379e2d3eafd5a970d0ede15b1c3939f7a22eb
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-06-25 23:14:45 +0900

    jsonの読み込み

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,12 +1,28 @@
 <template>
-  <p>Rockhopper-Penguin</p>
+  <div>
+    <p>{{ getJson }}</p>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
 
 @Component
-export default class Profile extends Vue {}
+export default class Profile extends Vue {
+  private getJson: any[] = [];
+
+  created() {
+    this.getDatas();
+  }
+
+  private async getDatas() {
+    const res = await axios.get("json/introduction.json").then((response) => {
+      this.getJson = response.data;
+    });
+    console.log("getData ->", this.getJson);
+  }
+}
 </script>
 
 <style></style>
