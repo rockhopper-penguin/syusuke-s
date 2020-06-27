5a1c0db376873199a4b769efce6ef06de4799f86
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-06-27 14:25:59 +0900

    index.tsの作成

---
+++ api/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app: express.Express = express();
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  return res.send("Rockhopper-Penguin");
+});
+
+app.get("/data", (req: express.Request, res: express.Response) => {
+  const data: object = [
+    {
+      name: "Rockhopper-Penguin",
+      Github_url: "https://github.com/rockhopper-penguin",
+      using_language: "TypeScript",
+    },
+  ];
+  return res.send(JSON.stringify(data));
+});
+
+app.listen(3000);
+console.log("Server running port 3000");
+
+export default app;
