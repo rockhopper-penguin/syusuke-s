d93a83ea3528458464c0de4cf8f7bbb59ad8c59b
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-07-09 21:53:33 +0900

    リンクの追加

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
     <p>Links</p>
+    <div>
+      <p>
+        Bitbucket
+        <a href="https://bitbucket.org/Rockhopper-Penguin/">rockhopper-penguin</a>
+      </p>
+      <p>
+        connpass
+        <a href="https://connpass.com/user/rockhopper-penguin/">瀬戸秀輔</a>
+      </p>
+      <p>
+        Doorkeeper：
+        <a href="https://www.doorkeeper.jp/users/penguin">瀬戸秀輔</a>
+      </p>
+      <p>
+        FaceBook：
+        <a href="https://www.facebook.com/penguin.s.seto">瀬戸秀輔</a>
+      </p>
+      <p>
+        GitHub：
+        <a href="https://github.com/rockhopper-penguin">Rockhopper-Penguin</a>
+      </p>
+      <p>
+        Qiita：
+        <a href="https://qiita.com/rockhopper-penguin">@rockhopper-penguin</a>
+      </p>
+    </div>
   </div>
 </template>
 
