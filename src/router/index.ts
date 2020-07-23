8a04b3a784a48ff2fe1d80b5695e60b1af79669a
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-07-23 23:08:32 +0900

    Portfolioページの作成・追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import { BootstrapVue } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import Links from "@/views/Links.vue";
+import Portfolio from "@/views/Portfolio.vue";
 
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
@@ -13,7 +14,7 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
     path: "/about",
@@ -22,17 +23,22 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/portfolio",
+    name: "Portfolio",
+    component: Portfolio,
   },
   {
     path: "/links",
     name: "Links",
-    component: Links
-  }
+    component: Links,
+  },
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
 });
 
 export default router;
