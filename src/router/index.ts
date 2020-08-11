58d29e5590a7119ee3ab0d0cedb59495ca4bafe6
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-08-11 22:55:04 +0900

    lint

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,7 +14,7 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Home
   },
   {
     path: "/about",
@@ -23,22 +23,22 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
   {
     path: "/portfolio",
     name: "Portfolio",
-    component: Portfolio,
+    component: Portfolio
   },
   {
     path: "/links",
     name: "Links",
-    component: Links,
-  },
+    component: Links
+  }
 ];
 
 const router = new VueRouter({
-  routes,
+  routes
 });
 
 export default router;
